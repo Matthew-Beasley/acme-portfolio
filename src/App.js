ade79e25d40d106c91963766c49739bba1c87401
conbec <56614504+conbec@users.noreply.github.com>
2020-01-30 19:13:08 -0800

    getting useer, and display code started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
+
+const fetchUser = async () => {
+  const API = 'https://acme-users-api-rev.herokuapp.com/api';
+  const storage = window.localStorage;
+  const userId = storage.getItem('userId');
+  if (userId) {
+    try {
+      return (await axios.get(`${API}/users/detail/${userId}`)).data;
+    }
+    catch (ex) {
+      storage.removeItem('userId');
+      return fetchUser();
+    }
+  }
+  const user = (await axios.get(`${API}/users/random`)).data;
+  storage.setItem('userId', user.id);
+  return user;
+};
+
+
+
 function App() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    fetchUser()
+      .then(response => setUser(response))
+  },[])
+console.log(user)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={user.avatar} alt=""/>
       </header>
+      
     </div>
   );
 }
